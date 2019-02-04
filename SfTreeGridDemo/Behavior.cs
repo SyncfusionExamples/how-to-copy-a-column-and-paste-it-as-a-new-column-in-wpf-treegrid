cc3b8f51c159bfe2c224d0911d531afd2caf93ef
saravanan.ayyanar <saravanan.ayyanar@syncfusion.com>
2019-02-04 17:35:22 +0530

    How to copy a column and paste it as a new column in wpf treegrid

---
+++ SfTreeGridDemo/Behavior.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.UI.Xaml.TreeGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using Syncfusion.UI.Xaml.TreeGrid;
+using System.Windows;
+using Syncfusion.Data;
+
+namespace SfTreeGridDemo
+{
+    class Behavior:Behavior<MainWindow>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+        }
+    }
+}
